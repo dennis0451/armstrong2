8616fc3171a50c6c5afcca9f473d0c05f3519633
Dennis Corral <dennis.corral@outlook.com>
2022-05-26 21:07:25 -0400

    solution with exponents

--- js/armstrongNumbers.js
+++ js/armstrongNumbers.js
@@ -1,5 +1,24 @@
 // How can you make this more scalable and reusable later?
 
-exports.findArmstrongNumbers = function() {
+exports.findArmstrongNumbers = function(arr) {
+    let armstrongNums = []
+
+    for(let j = 0; j < arr.length; j++){
+        let temp = arr[j].toString().split('')
+        let sum = []
+        let armstrongCheck = 0
+
+        for(let i = 0; i < temp.length; i++){
+            sum.push(Math.pow(temp[i],temp.length))
+        }
+        armstrongCheck = sum.reduce(function (a,b){return a + b})
+        if(armstrongCheck === arr[j]){
+            armstrongNums.push(arr[j])  
+        }
+    }    
+    return armstrongNums
 
 };
+
+
+
